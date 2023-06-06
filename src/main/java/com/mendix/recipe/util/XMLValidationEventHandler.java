d065e9ace2018537ce0d88fe7714df749b052e04
HITITDC\mesut.acar <mesut.acar@hititcs.com>
2023-06-06 09:44:25 +0300

    validation + logging

--- src/main/java/com/mendix/recipe/util/XMLValidationEventHandler.java
+++ src/main/java/com/mendix/recipe/util/XMLValidationEventHandler.java
@@ -1,16 +1,20 @@
 package com.mendix.recipe.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.xml.bind.ValidationEvent;
 import jakarta.xml.bind.ValidationEventHandler;
 
 public class XMLValidationEventHandler implements ValidationEventHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public boolean handleEvent(ValidationEvent event) {
-        System.out.println("Event Info: " + event);
+        logger.info("XML Validation Event Info: " + event);
         if (event.getMessage().toLowerCase().contains("unexpected element"))
             return true;
         return false;
     }
-
 }
